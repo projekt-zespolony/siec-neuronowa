b4114b0f8ff895571926c282817ff22b6b1206e3
Dawid Dziurla <dawidd0811@gmail.com>
2020-05-26 17:54:08 +0200

    print better

--- model_test_drive.py
+++ model_test_drive.py
@@ -16,7 +16,9 @@ def main():
         [0, 30, 1005, 30, 20],
         [0, 30, 1000, 30, 20],
         [0, 30, 995, 30, 20],
-        [0, 30, 990, 30, 20]
+        [0, 30, 990, 30, 20],
+        [1590229879, 24.56, 1003.72, 46.86, 16.63], # 0 1
+        [1590249652, 24.83, 1001.85, 50.01, 53.24] # 1 1
     ]
 
     with tf.Session(graph = tf.Graph()) as sess:
@@ -24,7 +26,10 @@ def main():
         graph = tf.get_default_graph()
         x = graph.get_tensor_by_name("input_input:0")
         model = graph.get_tensor_by_name("output/Sigmoid:0")
-        print(sess.run(model, {x: data}))
+        y = sess.run(model, {x: data})
+
+        for n in range(len(y)):
+            print(data[n], y[n])
 
 if __name__ == '__main__':
     main()
